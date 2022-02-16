4486a7c810f0816fc0b35088c1cc4f8506e6d269
Amaya Kejriwal <amaya@Amayas-MacBook-Air.local>
2022-02-16 17:02:19 -0500

    uploaded most recent files

--- main.js
+++ main.js
@@ -1,42 +1,37 @@
 
 // function 1 -- counting number of clicks on circle 1
-function ClickCounter()
-{
+
+
+ function ClickCounter()
+ {
     let a = document.getElementById("number").textContent;
     let click = Number(a) + 1;
     document.getElementById("number").textContent = String(click);
-
 }
 
 const circle1 = document.getElementById("circle1");
 circle1.addEventListener('click', ClickCounter);
 
 
-// function 2 -- adding black border to circle 2 on mouseover 
-
-// define 2 functions -- hover over + add border
-// mouse out - removes the border 
-
-// class list to add a border 
+// function 2 & 3 -- adding and removing black border from circle
 
 function AddBorder()
 {
-
-document.getElementById("circle2").classList.add() //AddBorder - and color 
-
+    document.getElementById("circle2").classList.add("border: solid");
 }
 
-const circle2 = document.getElementById("circle2");
-circle2.addEventListener("mouseover", AddBorder); 
-
+// const circle2 = document.getElementById("circle2");
+circle2.addEventListener('mouseover', AddBorder); //mouse over
 
 function RemoveBorder()
-
 {
-
-
+    document.getElementById("circle2").classList.remove("border: solid");
 }
-// mouseover and mouseout (move out of the circle we want border gone)
+
+// const circle2 = document.getElementById("circle2");
+circle2.addEventListener('mouseout', RemoveBorder); // mouse out 
 
 
-// function 3 -- 
+
+
+
